32193f6f2cf8b1f7726022e1a7e6294374a85da0
Alan Jassiel Castro Melgoza <jassmel05@gmail.com>
2025-07-02 15:48:37 -0600

    Init commit

---
+++ app/src/main/java/com/ajcm/citysearch/ui/theme/Color.kt
@@ -0,0 +1,27 @@
+package com.ajcm.citysearch.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * Light colors
+ */
+val BackgroundLight = Color(0xFFF9F5EC)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+val OutlineLight = Color(0xFF000000)
+val SurfaceLight = Color(0xFFFFFFFF)
+val PrimaryTextLight = Color(0xFF000000)
+val SecondaryTextLight = Color(0xFF000000)
+
+/**
+ * Night colors
+ */
+val BackgroundNight = Color(0xFF746E5E)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val OutlineNight = Color(0xFFFFFFFF)
+val SurfaceNight = Color(0xFF000000)
+val PrimaryTextNight = Color(0xFFFFFFFF)
+val SecondaryTextNight = Color(0xFFFFFFFF)
