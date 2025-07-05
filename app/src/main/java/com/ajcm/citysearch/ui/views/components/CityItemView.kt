b0a15130a723f5f32d11bc18a99c5d45b413a6ae
Alan Jassiel Castro Melgoza <jassmel05@gmail.com>
2025-07-04 23:29:09 -0600

    [feat]: add search & items views

---
+++ app/src/main/java/com/ajcm/citysearch/ui/views/components/CityItemView.kt
@@ -0,0 +1,119 @@
+package com.ajcm.citysearch.ui.views.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ajcm.data_source_manager.repository.model.City
+import com.ajcm.data_source_manager.repository.model.Coordinate
+
+@Composable
+fun CityItemView(
+    city: City,
+    onClick: (id: Int) -> Unit,
+    onFavoriteClicked: (id: Int) -> Unit,
+) {
+    Surface(
+        modifier = Modifier
+            .padding(vertical = 8.dp, horizontal = 16.dp)
+            .fillMaxWidth(),
+        onClick = { onClick(city.id) },
+        shadowElevation = 4.dp,
+        border = BorderStroke(2.dp, MaterialTheme.colorScheme.onSurface),
+        shape = MaterialTheme.shapes.medium,
+        color = MaterialTheme.colorScheme.surface,
+        tonalElevation = 2.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(48.dp)
+                    .background(MaterialTheme.colorScheme.onSurface)
+            )
+
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth(0.85f),
+                verticalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(
+                    text = "${city.cityName}, ${city.countryCode}",
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        modifier = Modifier
+                            .size(12.dp),
+                        imageVector = Icons.Default.LocationOn,
+                        contentDescription = "Location Icon",
+                        tint = MaterialTheme.colorScheme.onSurface
+                    )
+
+                    Text(
+                        text = "${city.coordinates.latitude}, ${city.coordinates.longitude}",
+                        style = MaterialTheme.typography.bodySmall,
+                    )
+                }
+            }
+
+            IconButton (
+                onClick = { onFavoriteClicked(city.id) },
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(24.dp),
+                    imageVector = if (city.favorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                    contentDescription = "Favorite Icon",
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CityItemViewPreview() {
+    CityItemView(
+        city = City(
+            countryCode = "US",
+            cityName = "New York",
+            id = 1,
+            coordinates = Coordinate(latitude = 40.7128, longitude = -74.0060),
+            favorite = false
+        ),
+        onClick = {},
+        onFavoriteClicked = {}
+    )
+}
---
+++ app/src/main/java/com/ajcm/citysearch/ui/views/components/SearchBarView.kt
@@ -0,0 +1,171 @@
+package com.ajcm.citysearch.ui.views.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ajcm.citysearch.R
+
+@Composable
+fun SearchBarView(
+    modifier: Modifier = Modifier,
+    onSearch: (String) -> Unit,
+    onFilterFavorites: (Boolean) -> Unit
+) {
+    var filterFavorites by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        SearchFieldBarView(
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .padding(start = 16.dp)
+                .fillMaxWidth(0.82f),
+            onSearch = onSearch
+        )
+
+        IconButton(
+            onClick = {
+                filterFavorites = !filterFavorites
+                onFilterFavorites(filterFavorites)
+            },
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(24.dp),
+                imageVector = if (filterFavorites) {
+                    Icons.Default.Favorite
+                } else {
+                    Icons.Default.FavoriteBorder
+                },
+                contentDescription = "Clear Icon",
+                tint = MaterialTheme.colorScheme.onSurface
+            )
+        }
+    }
+}
+
+@Composable
+fun SearchFieldBarView(
+    modifier: Modifier = Modifier,
+    onSearch: (String) -> Unit,
+) {
+    var text by rememberSaveable { mutableStateOf("") }
+
+    Surface(
+        modifier = modifier,
+        shadowElevation = 4.dp,
+        border = BorderStroke(2.dp, MaterialTheme.colorScheme.onSurface),
+        shape = MaterialTheme.shapes.medium,
+        color = MaterialTheme.colorScheme.surface,
+        tonalElevation = 2.dp
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .size(52.dp)
+                    .background(MaterialTheme.colorScheme.onSurface),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(32.dp),
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Location Icon",
+                    tint = MaterialTheme.colorScheme.surface
+                )
+            }
+
+            BasicTextField(
+                modifier = Modifier
+                    .fillMaxWidth(0.83f)
+                    .padding(horizontal = 8.dp),
+                value = text,
+                onValueChange = {
+                    text = it
+                    onSearch(it)
+                },
+                singleLine = true,
+                decorationBox = { innerTextField ->
+                    if (text.isEmpty()) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            contentAlignment = Alignment.CenterStart
+                        ) {
+                            Text(
+                                text = stringResource(R.string.search_hint),
+                                style = MaterialTheme.typography.bodyLarge
+                            )
+                        }
+                    }
+                    innerTextField()
+                }
+            )
+
+            if (!text.isEmpty()) {
+                IconButton(
+                    onClick = {
+                        text = ""
+                        onSearch(text)
+                    },
+                    modifier = Modifier.padding(start = 8.dp)
+                ) {
+                    Icon(
+                        modifier = Modifier
+                            .size(24.dp),
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = "Clear Icon",
+                        tint = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SearchBarViewPreview() {
+    SearchBarView(
+        modifier = Modifier.fillMaxWidth(),
+        onSearch = {},
+        onFilterFavorites = {}
+    )
+}
