0001cca2d9667c6511995ec2b59c9f8336add125
Alan Jassiel Castro Melgoza <jassmel05@gmail.com>
2025-07-06 18:56:56 -0600

    [refactor]: fix landscape state

--- app/src/main/java/com/ajcm/citysearch/ui/MainAppContainer.kt
+++ app/src/main/java/com/ajcm/citysearch/ui/MainAppContainer.kt
@@ -1,9 +1,16 @@
 package com.ajcm.citysearch.ui
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -14,7 +21,9 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.ajcm.citysearch.ui.views.CitiesMainContainer
@@ -28,6 +37,8 @@ fun MainAppContainer() {
     val coroutineScope = rememberCoroutineScope()
     val isKeyboardOpen by keyboardAsState()
 
+    val orientation = LocalConfiguration.current.orientation
+
     LaunchedEffect(isKeyboardOpen) {
         if (isKeyboardOpen) {
             coroutineScope.launch {
@@ -36,23 +47,45 @@ fun MainAppContainer() {
         }
     }
 
-    BottomSheetScaffold(
+    Box(
         modifier = Modifier
-            .consumeWindowInsets(WindowInsets.ime),
-        scaffoldState = scaffoldState,
-        sheetContainerColor = MaterialTheme.colorScheme.background,
-        sheetShadowElevation = 8.dp,
-        sheetPeekHeight = 320.dp,
-        sheetContent = {
-            CitiesMainContainer()
-        },
-    ) { _ ->
+            .fillMaxSize()
+            .consumeWindowInsets(WindowInsets.systemBars)
+            .consumeWindowInsets(WindowInsets.navigationBars)
+    ) {
         MapContainerView(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize(),
         ) {
             coroutineScope.launch {
                 scaffoldState.bottomSheetState.partialExpand()
             }
+        }
+
+        Box (
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .fillMaxWidth(
+                    if (orientation == Configuration.ORIENTATION_PORTRAIT) 1f else 0.5f
+                )
+                .fillMaxHeight()
+                .align(
+                    if (orientation == Configuration.ORIENTATION_PORTRAIT) Alignment.BottomCenter
+                    else Alignment.CenterStart
+                ),
+        ) {
+            BottomSheetScaffold(
+                modifier = Modifier
+                    .consumeWindowInsets(WindowInsets.ime),
+                scaffoldState = scaffoldState,
+                sheetContainerColor = MaterialTheme.colorScheme.background,
+                sheetShadowElevation = 8.dp,
+                sheetPeekHeight = 320.dp,
+                sheetContent = {
+                    CitiesMainContainer()
+                },
+                content = {}
+            )
         }
     }
 }
